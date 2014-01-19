54a2dcf11fe4081df5a1f4d0ecde08a09fd710cf
Calvinhieu <calvin.nhieu@gmail.com>
2014-01-18 20:00:58 -0500

    restarting project

--- gen/com/main/dotcolonies/R.java
+++ gen/com/main/dotcolonies/R.java
@@ -27,11 +27,11 @@ public final class R {
         public static final int main_image=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080001;
+        public static final int container=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int main_fragment=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -39,7 +39,6 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int main_image_str=0x7f050002;
     }
     public static final class style {
         /** 
--- src/com/main/dotcolonies/MainActivity.java
+++ src/com/main/dotcolonies/MainActivity.java
@@ -1,20 +1,20 @@
 package com.main.dotcolonies;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main_fragment);
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		return true;
 	}
-
+	
 }
